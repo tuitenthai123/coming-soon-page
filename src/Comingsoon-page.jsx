84d853a4019ca443641f65be9dca98193fc01149
Tui Tên Thái <119864448+tuitenthai123@users.noreply.github.com>
2025-07-18 21:55:37 +0700

    Update Comingsoon-page.jsx

--- src/Comingsoon-page.jsx
+++ src/Comingsoon-page.jsx
@@ -10,7 +10,7 @@ const Comingsoon
 
                  <span class="text-2xl">
          nhà mơ ước
-         </span>
+         <span/>
         <span>
      My dream house would be in the countryside. It would be small but beautiful, with three rooms and a lovely garden. In the living room, I would prefer to have a comfortable sofa and a large television. The bedroom would have a warm bed and a small lamp. In the kitchen, there would be a table with some chairs and a big fridge. I would also grow colorful flowers and some trees in my garden. I hope to have a swing or a bench to sit on and enjoy the fresh countryside air. On weekends, I could have a picnic with my family or friends and create beautiful memories. I want to live there because it is quiet, comfortable, and helps me feel calm after a hard day.
           <span/>
@@ -22,14 +22,14 @@ const Comingsoon
         <span/>
                    <span class="text-2xl">
          chuyến đi
-         </span>
+         <span/>
         <span>
        Last summer, my family and I went on a wonderful trip to the beach. The weather was sunny and warm, perfect for outdoor fun. We swam in the sea, built sandcastles, and played ball games on the sand. The water was cool and very clear. We also ate delicious seafood at a small restaurant near the beach. I took many photos with my parents to remember the trip. In the evening, we walked along the shore, watched the beautiful sunset, and collected colorful seashells. I felt really happy and relaxed. It was one of the best times I’ve had with my family. I hope we can visit the beach again next summer and make more great memories together.
           <span/>
           
          <span class="text-2xl">
          bạn thân
-         </span>
+         <span/>
         <span>
         My best friend’s name is Tom. He is kind, funny, and always ready to help others. Whenever I have a problem or need support, Tom is there to help me. We study in the same class, so we meet every day at school. After class, we often do homework together or play music, which is something we both enjoy. On weekends, we sometimes go to the park or ride our bikes around the neighborhood. Tom always knows how to make me laugh, and I feel happy when I’m with him. He listens carefully when I talk and truly cares about his friends. I’m really glad to have a best friend like Tom. He means a lot to me.
           <span/>
